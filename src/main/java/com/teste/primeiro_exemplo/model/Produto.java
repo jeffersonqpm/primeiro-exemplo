85b492e4dd6d604d915f4b9a917e2db654fce744
jeffersonqpm <jefferson.qpm@gmail.com>
2026-02-03 16:00:14 -0300

    ProdutoRepository

--- src/main/java/com/teste/primeiro_exemplo/model/Produto.java
+++ src/main/java/com/teste/primeiro_exemplo/model/Produto.java
@@ -8,50 +8,38 @@ public class Produto {
     private String nome;
     private int quantidade;
     private double valor;
-    private String obs;
+    private String observacao;
 
-    // #endregion
-
-    // #region getterESetters
-
+    
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public int getQuantidade() {
         return quantidade;
     }
-
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-
     public double getValor() {
         return valor;
     }
-
     public void setValor(double valor) {
         this.valor = valor;
     }
-
-    public String getObs() {
-        return obs;
+    public String getObservacao() {
+        return observacao;
     }
-
-    public void setObs(String obs) {
-        this.obs = obs;
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
     }
 
     // #endregion
